38e56c8f2214d9eff6879e245ecdbac95a4f87fd
MdMahbub-h <2021339001@student.sust.edu>
2025-07-03 06:07:59 +0600

    Update MenuScene.js

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -14,28 +14,28 @@ export class MenuScene extends Scene {
     }
 
     create() {
-        // this.scene.start("BootScene");
-        const video = this.add.video(
-            this.scale.width / 2,
-            this.scale.height / 2,
-            "intro"
-        );
-        video.setOrigin(0.5, 0.5);
-        video.setLoop(false);
-        video.setDisplaySize(this.scale.width / 7, this.scale.height / 4);
-        video.play();
+        this.scene.start("BootScene");
+        // const video = this.add.video(
+        //     this.scale.width / 2,
+        //     this.scale.height / 2,
+        //     "intro"
+        // );
+        // video.setOrigin(0.5, 0.5);
+        // video.setLoop(false);
+        // video.setDisplaySize(this.scale.width / 7, this.scale.height / 4);
+        // video.play();
 
-        video.on("complete", () => {
-            this.scene.start("BootScene");
-        });
+        // video.on("complete", () => {
+        //     this.scene.start("BootScene");
+        // });
 
-        this.scale.on("resize", (gameSize) => {
-            const { width, height } = gameSize;
-            video.setDisplaySize(width, height);
-        });
+        // this.scale.on("resize", (gameSize) => {
+        //     const { width, height } = gameSize;
+        //     video.setDisplaySize(width, height);
+        // });
 
-        if (this.isMobile) {
-            video.setScale(0.4);
-        }
+        // if (this.isMobile) {
+        //     video.setScale(0.4);
+        // }
     }
 }
